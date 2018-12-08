2101f288d5527579f0adff5db376b544bd8cd129
Havey3 <45610955+Havey3@users.noreply.github.com>
2018-12-08 10:08:22 -0500

    javascript exercise first upload

---
+++ script.js
@@ -0,0 +1,32 @@
+alert("Hello World!")
+
+var firstName = "Joshua";
+console.log (firstName)
+var lastName = "Havey";
+console.log (lastName)
+var age = (22);
+console.log(age)
+likesTravle = "true";
+
+var movieArray = ["Whiplash", "The Godfather"];
+console.log(movieArray [0])
+console.log(movieArray [1])
+
+var williamGrades = [62, 97, 99, 85, 73, 97];
+console.log(williamGrades [0])
+console.log(williamGrades [5])
+
+var movieStar = {
+    firstName: "Leonardo",
+    lastName: "DiCaprio",
+    age: 44,
+    favoriteFood: "Pizza"
+}
+
+var fullName = movieStar.firstName + " " + movieStar.lastName;
+var movieSentence = "Leonardo DiCaprio won his first oscar for his role in the movie 'The Revenant'.  He had been nominated six times."
+
+var historyLeo = movieStar.firstName + " " + movieStar.lastName + " " + movieSentence;
+console.log(movieStar.favoriteFood)
+console.log(fullName)
+console.log(historyLeo)
